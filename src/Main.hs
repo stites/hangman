7cf07c58aa3fbcd393eb49eff4f50ef912bfa539
stites <sam@stites.io>
2016-01-06 14:00:57 -0800

    go over buffering and add prompt

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,21 @@
 module Main where
 
+import System.IO
+
 import Hello
 
 main :: IO ()
 main = do
+  {- This way the next line will not be buffered/deferred. Otherwise, instead of:
+   - $ Please input your name: <input>
+   - >>> Hello <input>!
+   -
+   - We will get something like:
+   - $ <input>
+   - >>> Please input your name: Hello <input>!
+   -}
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
   name <- getLine -- getLine :: IO String
   sayHello name
 
