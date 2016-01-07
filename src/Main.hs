d7080e5a85d859bb0c02a5578f5c77d312cf4fa2
stites <sam@stites.io>
2016-01-07 07:13:20 -0800

    make random word generator

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,40 @@
 module Main where
 
-import System.IO
+import Control.Monad (forever)     -- infinite loop, not necessary
+import Data.Char     (toLower)
+import Data.Maybe    (isJust)
+import Data.List     (intersperse)
+import System.Exit   (exitSuccess)
+import System.Random (randomRIO)
 
-import Hello
+type WordList = [String]
 
-main :: IO ()
-main = do
-  {- This way the next line will not be buffered/deferred. Otherwise, instead of:
-   - $ Please input your name: <input>
-   - >>> Hello <input>!
-   -
-   - We will get something like:
-   - $ <input>
-   - >>> Please input your name: Hello <input>!
-   -}
-  hSetBuffering stdout NoBuffering
-  putStr "Please input your name: "
-  name <- getLine -- getLine :: IO String
-  sayHello name
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  all <- allWords
+  return $ filter gameLength all
+    where gameLength w =
+            let l = length (w::String)
+            in l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIdx <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIdx
+
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
 
